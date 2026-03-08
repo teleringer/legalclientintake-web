18daf8440d00af1fc255f8f278e6ec94d42e438d
Dumpsternator Lab <dumpsternator@gmail.com>
2026-03-08 12:44:55 -0400

    Fix maskable icon manifest

--- app/manifest.ts
+++ app/manifest.ts
@@ -20,7 +20,7 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/lci-icon-512.png",
         sizes: "512x512",
         type: "image/png",
-        purpose: "any maskable",
+        purpose: "maskable",
       },
     ],
   };
