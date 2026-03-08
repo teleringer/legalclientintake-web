3458d60e8eab0405d0da2de9b52687f5b044f450
Dumpsternator Lab <dumpsternator@gmail.com>
2026-03-08 12:47:06 -0400

    Fix both manifest icon purpose values

--- app/manifest.ts
+++ app/manifest.ts
@@ -14,7 +14,7 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/lci-icon-192.png",
         sizes: "192x192",
         type: "image/png",
-        purpose: "any maskable",
+        purpose: "maskable",
       },
       {
         src: "/lci-icon-512.png",
